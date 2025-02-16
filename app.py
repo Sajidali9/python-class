4cb5cb68e7f22396bddd2dabaaa10517f84269bb
sajid ali <Sajid.110.dijas@gmail.com>
2025-02-16 21:38:17 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-print("Hello World")
-print("hi iam sajid ali\ and iam 28 year old from karachi.")
-a = 110
-print(a)
-a1 = 14
-b = "sajid"
-print(b)
-c = True
-d = None
-print(a + a1)
+print("veriable")
+
+Name: str="Sajid ali"
+Age: int = "28"
+active : bool="True"
+
+print("Name","Sajid ali")
+print("Age", "28")
+
+print("The type of a is",type(a))
+print("the tpye of b is",type(b))
+print("the type of c is",type(c))
+print("yhe type of d is",type(d))
