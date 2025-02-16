1493aec8869dc2063284c47b55a9df3191a979f8
sajid ali <Sajid.110.dijas@gmail.com>
2025-02-16 21:58:33 +0500

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,13 @@
+print("veriable")
+
+Name: str="Sajid ali"
+Age: int = "28"
+active : bool="True"
+
+print("Name","Sajid ali")
+print("Age", "28")
+
+print("The type of a is",type(a))
+print("the tpye of b is",type(b))
+print("the type of c is",type(c))
+print("yhe type of d is",type(d))
