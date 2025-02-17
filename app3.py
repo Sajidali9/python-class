2e03d0944cfe298af9a53c309022d98883e34add
sajid ali <Sajid.110.dijas@gmail.com>
2025-02-17 06:26:16 +0500

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,83 @@
+print("veriable")
+
+Name: str="Sajid ali"
+Age: int = "28"
+active : bool="True"
+
+print("Name","Sajid ali")
+print("Age", "28")
+
+print("The type of a is",type(a))
+print("the tpye of b is",type(b))
+print("the type of c is",type(c))
+print("yhe type of d is",type(d))
+
+print("compairson operator || true\false")
+
+#==(equal to)
+#>(greater than equal)
+#<(less than equal)
+#>=(greater than equal)
+
+#<=(less than equal)
+#!=(not equal to)
+
+num1: int = 8
+num2: int = 1
+
+print(num1 == num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <=num2)
+print(num1 != num2)
+
+print("logical operator")
+
+num1: int = 10
+num2: int = 5
+print(num1 > num2 and num2< num1)
+
+#or operater
+
+num1: int = 40
+print(num1>10 and num1<30)
+#dono condition ma true honi chaiye.
+#true or false
+print(num1> 10 or num1>30)
+# koi bhe condition true hogi to answer bhe true hoga
+# And
+#num2 is less than 30
+#num1 >10 = True
+#num2 <30 = False
+# True or False
+print(num1 > 10)
+print(num1 > 20)
+print(num1 > 30)
+print(num1 < 10 and num1 > 20 and num1 <30)
+
+# 2 or operator
+num1: int = 40
+print (num1 > 10 and num1<30)
+print (num1 >10 or num1 > 30)
+#koi bhe condition true hogi tu answer bi true hoga.  
+
+# topic if or else agr codition ture hogi tou "if" lagay ga or agr condition false hogi tou "else" lagayga
+ #condition -> True/False
+if True:
+ print ("condition is true")
+else:
+  print("condition is false")
+
+num: int = 10
+print(num > 10)
+print(num  == 10)
+print(num < 100)
+print(num > 0 and num < 100)
+#if num > 10
+
+num2 = 1
+if num1 > num2:
+    print("Hello")
+    uname = "Sir Ameen"
+    print(uname)
